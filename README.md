9154ef175feccbc93bb7d537b49fcd4e5157727f
Meegan <matt.egan@layerframe.com>
2015-04-17 17:40:41 -0400

    First real commit!

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# mouse-parallax
-Track mouse position relative to a parent element and move it accordingly
+# mouse-parallax V 0.0.1
+Whee! here we go:
+
+This is the start of a JS experiement testing out tying mouse movements to various DOM elems wrapped in a parent. Trying to make this agnostic as I can, but there's some caveats:
+
+1. Only works on browsers with CSS transforms and querySelectorAll (throws error otherwise... will be addressed before it hits 0.1)
+2. No dependencies, all vanilla. Will probably work into a directive for Angular (probs 0.5?)
+
+So how do you use it? Great question!
+
+1. Get the main.js file in /dist
+2. Bring it into your local project, rename it to whatever your lil' heart desires
+3. Set up your DOM
+	a. Set a parent element to track mouse to (EX: <div>)
+	b. Give this parent the id of mouse-move-wrapper (EX: <div id="mouse-move-wrapper">)
+	c. For elements that move around a lot, give them a class of more-move (EX: <div class="move-more">)
+	d. For elements that move around less, give them a class of move-less (EX: <div class="move-less">)
+4. Set up your CSS
+	a. Set the parent-move-wrapper to a position of relative
+	b. Set the children to w/e (just not static, they need a position property!)
+	c. Position the element on init w/ top/right/bottom/left
+5. Set up Javascript
+	a. call the function jsMouseTrack(integer) to kick it off
+	b. I dunno, party?
+
+So the difference here is anything with a class of move-les will move half as much as move more
---
+++ app/scripts/main.js
@@ -0,0 +1,105 @@
+'use strict';
+
+console.log('\'Allo \'Allo!');
+
+var jsMouseTrack = function(drama) {
+
+	// Testing snippet, probably can be refactored out
+	function detectCSSFeature(featurename){
+		var feature = false,
+		domPrefixes = 'Webkit Moz ms O'.split(' '),
+		elm = document.createElement('div'),
+		featurenameCapital = null;
+
+		featurename = featurename.toLowerCase();
+
+		if( elm.style[featurename] !== undefined ) { feature = true; } 
+
+		if( feature === false ) {
+			featurenameCapital = featurename.charAt(0).toUpperCase() + featurename.substr(1);
+			for( var i = 0; i < domPrefixes.length; i++ ) {
+				if( elm.style[domPrefixes[i] + featurenameCapital ] !== undefined ) {
+					feature = true;
+					break;
+				}
+			}
+		}
+		return feature; 
+	}
+
+	// test browser support
+	if(!document.querySelectorAll || !detectCSSFeature('transform')) {
+
+		throw new Error('this browser doesn\'t support querySelectorAll or CSStransforms, sorry!');
+
+	}
+
+	// our moving pieces
+	var masterParent = document.querySelector('#mouse-move-wrapper');
+	var slowChildren = document.querySelectorAll('.less-move');
+	var quickChildren = document.querySelectorAll('.more-move');
+
+	
+	//Final Calcs we'll be using
+	var getMousePositionVsMasterParent = function(e){
+
+		// Mouse Pos
+		var xMouse = (e.clientX || e.pageX) - masterParent.offsetLeft;
+    var yMouse = (e.clientY || e.pageY) - masterParent.offsetTop;
+
+    // parent Pos
+    var xParent = masterParent.offsetWidth / 2;
+		var yParent = masterParent.offsetHeight / 2;
+
+    var posX = xMouse - xParent;
+    var posY = yMouse - yParent;
+
+    return {
+
+    	x: posX,
+    	y: posY
+
+    };
+
+	};
+
+	// Calcs done, let's eat some meat...
+	var moveElements = function(e, element, amt) {
+
+		// MAAAATH DAMN YOU
+		var finalAmt = amt;
+
+		// Alter how much they'll actually move
+		var posX = getMousePositionVsMasterParent(e).x / finalAmt;
+		var posY = getMousePositionVsMasterParent(e).y / finalAmt;
+
+		var transformString = ('translate(' + posX + 'px, ' + posY + 'px)');
+
+		element.style.webkitTransform = transformString;
+		element.style.MozTransform = transformString;
+		element.style.msTransform = transformString;
+		element.style.OTransform = transformString;
+		element.style.transform = transformString;
+
+	};
+
+	masterParent.addEventListener('mousemove', function(e){
+
+		for(var i = 0; i < slowChildren.length; i++){
+
+			moveElements(e, slowChildren[i], drama);
+
+		}
+
+		// God dammit JShint
+		for(var j = 0; j < quickChildren.length; i++){
+
+			moveElements(e, quickChildren[j], (drama /2));
+
+		}
+
+	});
+
+};
+
+jsMouseTrack(30);
